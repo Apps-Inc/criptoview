1ae0976eee0ed8a8d8aad32acdd3bfd2a87a6ebe
Aníbal Vilela <anibal.vilela@vaivoa.com>
2021-11-30 15:29:28 -0300

    Projeto inicial

---
+++ criptoview/src/AppDelegate.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // TODO: Do we need this?
+    //var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // TODO: Do we need this?
+
+        //window = UIWindow(frame: UIScreen.main.bounds)
+        //window?.rootViewController = ViewController()
+        //window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
