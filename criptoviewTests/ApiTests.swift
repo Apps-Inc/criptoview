38d1fabc23ec3d01d3b7d9be9be9a0c735c86dff
Aníbal Vilela <anibal.vilela@vaivoa.com>
2021-12-02 08:57:51 -0300

    Adicionar chave para testes

--- criptoviewTests/ApiTests.swift
+++ criptoviewTests/ApiTests.swift
@@ -6,7 +6,9 @@ class ApiTests: XCTestCase {
     var api: NomicsAPI!
 
     override func setUp() {
-        api = NomicsAPI(key: "b91b2ed6c75004f22ab854565929e843b9b8ae38")
+        let nomicsKey = ProcessInfo.processInfo.environment["TESTS_NOMICS_API_KEY"]!
+
+        api = NomicsAPI(key: nomicsKey)
     }
 
     override func setUpWithError() throws {
