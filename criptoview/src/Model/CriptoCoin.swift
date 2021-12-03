8750a9c94bf41547ad7b185feb0b76ec20d105c5
Victor Mello <victor.mello@vaivoa.com>
2021-12-03 10:00:13 -0300

    new coins

--- criptoview/src/Model/CriptoCoin.swift
+++ criptoview/src/Model/CriptoCoin.swift
@@ -2,9 +2,19 @@ import Foundation
 
 enum CriptoCoin: String, CaseIterable {
     case BTC = "Bitcoin"
-    case DOGE = "Dogecoin"
     case ETH = "Etherium"
+    case BNB = "Binance Coin"
+    case USDT = "Tether"
     case XMR = "Monero"
+    case XRP = "XRP"
+    case USDC = "USD Coin"
+    case DOT = "Polkadot"
+    case DOGE = "Dogecoin"
+    case AVAX = "Avalanche"
+    case LTC = "Litecoin"
+    case TRON = "TRON"
+    case DAI = "Dai"
+    case XLM = "Stellar"
 
     var symbol: String { String(describing: self) }
 
